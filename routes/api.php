e97eddca9e257b7fee33003ef2ab69de9a4cd356
dnieafeeq <76414745+dnieafeeq@users.noreply.github.com>
2021-10-12 17:39:31 +0800

    saya tak cukup power spec
    nak kena tambah power.

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\AduanController;
-Route::resource('aduan', AduanController::class);
+use App\Http\Controllers\ApiAduanController;
+Route::resource('aduan', ApiAduanController::class);
 
-use App\Http\Controllers\JalanController;
-Route::resource('jalan', JalanController::class);
+use App\Http\Controllers\ApiJalanController;
+Route::resource('jalan', ApiJalanController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
