508fa96dad1912c374a400b97396e0d49ca57a8c
Dinie Afeeq <dinieafiq@pipeline.com.my>
2021-10-20 11:37:56 +0800

    ,,

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ApiAduan;
 use App\Http\Controllers\AuthController;
+use App\Models\User;
 
 Route::resource('aduan', ApiAduan::class);
 
