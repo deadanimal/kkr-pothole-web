af7e4a43777e24d15b7f332f94fbfe38742e3f58
AmNaqib <naqib.zaidi@pipeline-network.com>
2021-11-30 17:19:29 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/success', function () {
+    return view('success');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
